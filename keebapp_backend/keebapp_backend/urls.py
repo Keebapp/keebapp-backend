a632eaa4298d03cebd141d82aa73f16d067839ad
leviwaiu <contact@leviwaiu.com>
2021-08-21 07:58:58 +0100

    Adding rest framework setup.

--- keebapp_backend/keebapp_backend/urls.py
+++ keebapp_backend/keebapp_backend/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    path('api-auth/', include('rest_framework.urls')),
     path('admin/', admin.site.urls),
     path('groupbuy/', include('groupbuy.urls'))
 ]
